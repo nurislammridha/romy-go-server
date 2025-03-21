4cd6312d56232c43b5aedc391c59e1d5c9be35bd
nurislammridha <mridha851@gmail.com>
2025-03-21 18:05:35 +0600

    chatting..

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ const PORT = 4000;
 let activeDrivers = {};
 console.log('234', 123)
 io.on('connection', (socket) => {
-    console.log('111', 111)
     console.log('A user connected:', socket.id);
 
     // When a driver comes online
@@ -53,6 +52,25 @@ io.on('connection', (socket) => {
         console.log('User disconnected:', socket.id);
         delete activeDrivers[socket.id];
     });
+
+
+    //CHATTING FUNCTIONALITY START HERE
+    // Join a room for private chat
+    socket.on('joinRoom', ({ userId }) => {
+        socket.join(userId); // userId acts as room ID
+    });
+    // Handle sending messages
+    socket.on('sendMessage', async ({ senderId, receiverId, message, type }) => {
+        const newMessage = new Message({ senderId, receiverId, message, type });
+        // await newMessage.save();
+
+        // Emit to the receiver's room
+        io.to(receiverId).emit('receiveMessage', newMessage);
+    });
+    socket.on('disconnect', () => {
+        console.log('Client disconnected:', socket.id);
+    });
+    //CHATTING FUNCTIONALITY END HERE
 });
 // io.on("connection", (socket) => {
 //     console.log("A user connected:", socket.id);
