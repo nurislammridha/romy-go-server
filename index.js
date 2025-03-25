3845147bd8a7df65fff87e50bf3a0199920f320a
nurislammridha <mridha851@gmail.com>
2025-03-25 14:12:03 +0600

    audio..

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const http = require("http");
+const { RtcTokenBuilder, RtcRole } = require("agora-access-token");
 const { Server } = require("socket.io");
 
 const app = express();
+app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
@@ -11,6 +13,28 @@ const io = new Server(server, {
     }
 });
 const PORT = 4000;
+const AGORA_APP_ID = "020850dba286443a8a3a6a7cc6435da4";
+const AGORA_APP_CERTIFICATE = "89fb86e6c8054ec28b6c067eeca6c700";
+//AGORA START HERE
+// Function to generate Agora token
+app.post("/get-token", (req, res) => {
+    const { channelName, uid } = req.body;
+    const expirationTime = 3600; // 1 hour
+
+    const token = RtcTokenBuilder.buildTokenWithUid(
+        AGORA_APP_ID,
+        AGORA_APP_CERTIFICATE,
+        channelName,
+        uid,
+        RtcRole.PUBLISHER,
+        expirationTime
+    );
+
+    res.json({ token });
+});
+//AGORA END HERE
+
+
 //cors policy
 // app.use(cors());
 // app.options("*", cors());
@@ -56,9 +80,9 @@ io.on('connection', (socket) => {
 
     //CHATTING FUNCTIONALITY START HERE
     // Join a room for private chat
-    socket.on('joinRoom', ({ userId }) => {
-        socket.join(userId); // userId acts as room ID
-    });
+    // socket.on('joinRoom', ({ userId }) => {//off for calling 
+    //     socket.join(userId); // userId acts as room ID
+    // });
     // Handle sending messages
     socket.on('sendMessage', async ({ senderId, receiverId, message, type }) => {
         // const newMessage = new Message({ senderId, receiverId, message, type });
@@ -68,10 +92,27 @@ io.on('connection', (socket) => {
         // Emit to the receiver's room
         io.to(receiverId).emit('receiveMessage', newMessage);
     });
-    socket.on('disconnect', () => {
-        console.log('Client disconnected:', socket.id);
-    });
+    // socket.on('disconnect', () => {
+    //     console.log('Client disconnected:', socket.id);
+    // });
     //CHATTING FUNCTIONALITY END HERE
+    //CALLING FUNCTION START HERE
+    socket.on("callUser", ({ userToCall, channelName, callerId }) => {
+        io.to(userToCall).emit("incomingCall", { channelName, callerId });
+    });
+
+    socket.on("joinRoom", (userId) => {
+        socket.join(userId);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+    });
+    //CALLING FUNCTION END HERE
+
+
+
+
 });
 // io.on("connection", (socket) => {
 //     console.log("A user connected:", socket.id);
@@ -83,6 +124,10 @@ io.on('connection', (socket) => {
 app.get("/", (req, res) => {
     res.send("Server is working!");
 });
+//
+
+
+
 server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
